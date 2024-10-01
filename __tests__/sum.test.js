5daef674f2c999cefeba005b99c65ddc2eaa07b9
seppeverbeken <seppe.verbeken@student.ehb.be>
2024-10-01 14:56:46 +0200

    Remove failing tests

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -5,10 +5,4 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).not.toBe(4);
   expect(sum(1, 2)).toBeGreaterThan(2);
   expect(sum(1, 2)).toBeLessThan(4);
-
-//   write some failing tests as well
-    expect(sum(1, 2)).toBe(4);
-    expect(sum(1, 2)).not.toBe(3);
-    expect(sum(1, 2)).toBeGreaterThan(4);
-    expect(sum(1, 2)).toBeLessThan(2);
 });
