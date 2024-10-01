b4e12a50c0e753fa2b09f073fa2c5705e5d199ff
seppeverbeken <seppe.verbeken@student.ehb.be>
2024-10-01 14:54:44 +0200

    Test failing test cases

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,4 +2,13 @@ const sum = require('../sum.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).not.toBe(4);
+  expect(sum(1, 2)).toBeGreaterThan(2);
+  expect(sum(1, 2)).toBeLessThan(4);
+
+//   write some failing tests as well
+    expect(sum(1, 2)).toBe(4);
+    expect(sum(1, 2)).not.toBe(3);
+    expect(sum(1, 2)).toBeGreaterThan(4);
+    expect(sum(1, 2)).toBeLessThan(2);
 });
