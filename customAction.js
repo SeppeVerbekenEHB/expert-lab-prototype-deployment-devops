230c43adc20f92685a5e868065ec64967f215665
seppeverbeken <seppe.verbeken@student.ehb.be>
2024-10-09 14:13:20 +0200

    Adjust custom action

--- customAction.js
+++ customAction.js
@@ -1,19 +1,8 @@
 const core = require('@actions/core');
 
-async function run() {
-  try {
-    // Get the 'name' input defined in the action.yml
-    const name = core.getInput('name');
-    const message = `Hello, ${name}!`;
-
-    // Print the greeting message
-    console.log(message);
-
-    // Set the output message
-    core.setOutput('message', message);
-  } catch (error) {
-    core.setFailed(`Action failed with error: ${error.message}`);
-  }
+try {
+    const whoToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${whoToGreet}!`);
+} catch (error) {
+    core.setFailed(error.message);
 }
-
-run();
